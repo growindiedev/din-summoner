0e1d8b88604ff7f1fe8c4e134ff567f1c16e313a
Dekan <dekanbrown@odyssy.io>
2024-04-05 15:42:59 -0600

    update to 0.3

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,7 +11,7 @@ export const CURATOR_CONTRACTS: KeychainList = {
   NFT_CURATOR_SUMMONER: {
     // "0x1": "0xb5a8838d27df4633894feaa4e5b902a874ec8e7e",
     // "0x5": "0x759ef79cfcc4f155052554ca7ff9c39b0e76abd1",
-    "0xaa36a7": "0x70132cd79f90306bc68c1930f4364452a17aa552" // "0x4bb5d274dea3542bb836cba64bd7eed63233cb58"
+    "0xaa36a7": "0xD2bA2c50D16D35d6B8642A06b2C882F9fe790EDD", // "0x70132cd79f90306bc68c1930f4364452a17aa552" // "0x4bb5d274dea3542bb836cba64bd7eed63233cb58"
 
   },
   BASIC_HOS_SUMMONER: {
@@ -43,7 +43,7 @@ export const CURATOR_CONTRACTS: KeychainList = {
   },
   NFT_CURATOR_SINGLETON: {
     // "0x5": "0xb3dc8207075e9c7e78efb7148fb59b6f5da1dc28"
-    "0xaa36a7": "0xec0d1f8baed05ed6516fe0f9304566115f2dbbde", // "0xeece40a7492efe0598e466ff501530feaddd501d", // "0x31cb4b142ed082147d4c71da0c3bbef0b6c0f3ae"
+    "0xaa36a7": "0x5dbC54D909eA0dAFc42A08CAE140e46Af388d451", // "0xec0d1f8baed05ed6516fe0f9304566115f2dbbde", // "0xeece40a7492efe0598e466ff501530feaddd501d", // "0x31cb4b142ed082147d4c71da0c3bbef0b6c0f3ae"
   },
   CLAIM_SHAMAN_SINGLETON: {
     // "0x1": "0x80daee7a5322fbeb3f0f72531fdab5ed0f3e7333",
